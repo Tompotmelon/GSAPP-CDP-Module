44319e50edb3f353b28ceb9c841b8dbfeacdd5ef
Tompotmelon <135347096+Tompotmelon@users.noreply.github.com>
2023-07-02 08:11:37 -0400

    Data and APIs
    Interactive Web submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,6 +1,10 @@
 console.log("this works");
 console.log("Hello, World!")
 const API_KEY = config.WEATHER_API_KEY;
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
+function getWeatherData() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
 .then(response => response.json())
 .then(data => console.log(data));
+}
+
+getWeatherData()
