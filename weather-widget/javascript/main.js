6f41dca1228037816afd448647a495c5cdea6042
Tompotmelon <135347096+Tompotmelon@users.noreply.github.com>
2023-07-02 05:29:05 -0400

    Update main.js
    OpenWeather API call

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,3 +1,6 @@
 console.log("this works");
 console.log("Hello, World!")
 const API_KEY = config.WEATHER_API_KEY;
+fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
+.then(response => response.json())
+.then(data => console.log(data));
